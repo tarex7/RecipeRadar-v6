020fcb29f00cd6efc656b9019bef0500737f16e3
tarex7 <tarex7@gmail.com>
2024-01-30 10:04:57 +0100

    start project - vu3 - vuetify ok

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <Head title="Welcome" />
+
+    <v-app>
+        <v-container>
+          <v-btn color="primary">Primary Button</v-btn>
+          <v-text-field label="Label"></v-text-field>
+        </v-container>
+      </v-app>
+</template>
+
+<style>
+
+</style>
---
+++ resources/js/plugins/vuetify.js
@@ -0,0 +1,10 @@
+// resources/js/plugins/vuetify.js
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export default createVuetify({
+  components,
+  directives,
+  // Qui puoi aggiungere le tue configurazioni tematiche o altro...
+})
